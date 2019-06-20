cf1f08d4288fff18d37327d116f70c7db3cfd889
Baopqspkt <44236073+Baopqspkt@users.noreply.github.com>
2019-06-20 23:39:32 +0700

    Add GUI

--- encode/G4-4.py
+++ encode/G4-4.py
@@ -7,6 +7,7 @@ import argparse
 import pickle
 import cv2
 import os
+from pexpect import pxssh
 
 root  = Tk()
 root.title("Encode file")
@@ -14,7 +15,10 @@ root.geometry("400x400+100+50")
 
 a = StringVar()
 b = StringVar()
-t = Text(root,height=400,width=40)
+c = StringVar()
+d = StringVar()
+e = StringVar()
+t = Text(root,height=400,width=100)
 
 def dataset_adress():
 	c = a.get()
@@ -26,6 +30,13 @@ def encode_adress():
     c = c.replace('\\','/')
     return c
 
+def ExitApplication():
+	MsgBox = tkMessageBox.askquestion('Exit Application', 'Are you want to exit', icon='warning')
+	if MsgBox == 'yes':
+		root.destroy()
+	else:
+		tkMessageBox.showinfo('Return', 'You will now return to the application screen')
+
 def clicked():
 	check = 0
 	c = dataset_adress()
@@ -35,6 +46,8 @@ def clicked():
 	print (d)
 	if os.path.isdir(c) and os.path.isdir(d):
 		check = 1
+	else:
+		tkMessageBox.showerror('Information','Link error please fillout')
 	if check == 1:
 		imagePaths = list(paths.list_images(c))
 		knownEncodings = []
@@ -59,10 +72,20 @@ def clicked():
 		f = open(d + '/' + "encoding.pickle", "wb")
 		f.write(pickle.dumps(data))
 		f.close()
-	if (check == 1):
-		tkMessageBox.showinfo('Information','Complie complette')	
-	else:
-		tkMessageBox.showerror('Information','Link error please input by hand')
+		#tkMessageBox.showinfo('Information','Complie complette')
+		#tkMessageBox.showinfo('Information','Complie complette')
+		s = pxssh.pxssh()
+		hostname = c.get()
+		username = d.get()
+		password = e.get()
+		s.login(hostname,username,password,sync_multiplier=5, auto_prompt_reset=False)
+		os.system("/Users/bpham/Desktop/code-python/scp.command")
+		s.sendline('sudo reboot -h now')
+		s.prompt()
+		s.logout()
+		tkMessageBox.showinfo('Information','Complie complette')
+			
+	
 noidung = """ Encode  file from image to file pickle.
 1. Select path of image file
 2. Select path of output file
@@ -70,15 +93,22 @@ noidung = """ Encode  file from image to file pickle.
 4. Press Quit to exit process
 Let's start
 Author: baopq.spkt@gmail.com """
+
+## Component
 img =ImageTk.PhotoImage(Image.open('logo.png'))
-Button(root, text='Quit', command=root.destroy).pack(fill=X)
+Button(root, text='Quit', command=ExitApplication, fg = "Red").pack(fill=X)
 Label(root,text='Link dataset').pack(fill=X)
 Entry(root,textvariable=a).pack(fill=X)
 Label(root,text='Adress File output').pack(fill=X)
 text = Entry(textvariable=b).pack(fill=X)
 Button(root, text='Encode File', command=clicked).pack(fill=X)
-panel = Label(root,image = img)
-panel.pack(side = "bottom",fill = "both", expand = "yes")
+Label(root,text='Your IP').pack(fill=X)
+text = Entry(textvariable=c).pack(fill=X)
+Label(root,text='User Name').pack(fill=X)
+text = Entry(textvariable=d).pack(fill=X)
+Label(root,text='Password').pack(fill=X)
+text = Entry(textvariable=e).pack(fill=X)
+
 t.pack(fill=X)
 t.insert(END,noidung)
 
